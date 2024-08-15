98b67f8e50122291bf7740281a0c5b7d41aef75c
Yash <yashsavani19@gmail.com>
2024-08-15 15:31:09 +1200

    Jest Test Examples Written and succesfully passing

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+
+describe('Addition', () => {
+  it('2 + 2 should be 4', () => {
+  expect(2 + 2).toBe(4);
+  });
+  });
+  
