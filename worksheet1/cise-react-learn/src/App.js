ad28f4855e14c82814a37a2e61580a0547194f86
Yash <yashsavani19@gmail.com>
2024-08-16 19:52:40 +1200

    Changing worksheet1 app,js back to default

--- worksheet1/cise-react-learn/src/App.js
+++ worksheet1/cise-react-learn/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+<body>
+      <div>
+        <div class="App">
+          <header class="App-header">
+            <img src="logo.svg" class="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              class="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </div>
+    </body>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.aut.ac.nz"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Agile is about Values and Principles
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
